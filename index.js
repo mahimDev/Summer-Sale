0f882ef522b6e8347a9a04f55a82050a1de566c7
MD Mahim <138321905+mahim7172@users.noreply.github.com>
2023-08-20 05:39:49 +0600

    added main section

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 let total = 0;
-function clickElementValue(target){
-const elementValue = document.getElementById('input-field');
-const item = target.childNodes[3].childNodes[3].innerText
+function clickElementValue(target) {
+  const elementValue = document.getElementById("input-field");
+  const item = target.childNodes[3].childNodes[3].innerText;
 
- const li = document.createElement('li')
- li.innerText = item
- elementValue.appendChild(li)
+  const li = document.createElement("li");
+  li.innerText = item;
+  elementValue.appendChild(li);
 
-const price = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
+  const price = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
 
-total = parseFloat(total) + parseFloat(price)
+  total = parseFloat(total) + parseFloat(price);
 
-const totalPrice = document.getElementById('total-price').innerText = total
+  const totalPrice = (document.getElementById("total-price").innerText = total);
 
-const discountPrice = totalPrice / 100;
-const discount = discountPrice * 20
-const discountTotal = document.getElementById('discount-price').innerText = discount;
+  const discountPrice = totalPrice / 100;
+  const discount = discountPrice * 20;
+  const discountTotal = (document.getElementById("discount-price").innerText =
+    discount);
 
-const productPrice = total - discountTotal;
+  const productPrice = total - discountTotal;
 
-const product = document.getElementById('product-price').innerText = productPrice
-return product;
+  const product = (document.getElementById("product-price").innerText =
+    productPrice);
 
+  const btn = document.getElementById("parchase-btn");
+  console.log(btn);
+  if (totalPrice > 0) {
+    btn.setEnabled(true);
+  }
+  return product;
 }
